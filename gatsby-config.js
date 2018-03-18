96c1af135ceafade17ad90915a884cb1124ce554
Joshua Friend <josh.friend@daveramsey.com>
2018-03-17 22:53:41 -0500

    Set dummy podcast data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
+let authors = 'Josh and Ashley Friend';
+
 module.exports = {
   siteMetadata: {
     title: 'Living a Life with Friends',
     description: `Everyday Ordinary Friends where Every Day is a Party.`,
-    siteUrl: `https:livingalifewithfriends.com`,
+
+    //-- Podcast Definitions
+    siteUrl: `https://livingalifewithfriends.com`,
+
+    podcastInformation: {
+      title: 'Stressed to Blessed',
+      url: `https://livingalifewithfriends.com/podcast`,
+      description: `Something serios`,
+    }
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -42,14 +52,69 @@ module.exports = {
         {
           site {
             siteMetadata {
-              title
-              description
               siteUrl
-              site_url: siteUrl
+              podcastInformation {
+                title
+                description
+                url
+              }
             }
           }
-        }
-      `,
+        }`,
+        setup: ({ query: { site: { siteMetadata }, ...rest } }) => {
+
+          let podcastMetadata = {
+            generator: 'JoshWhatK Podcast RSS Generator',
+            title: siteMetadata.podcastInformation.title,
+            description: siteMetadata.podcastInformation.description,
+            site_url: siteMetadata.podcastInformation.url,
+            language: 'en',
+            copyright: `Josh & Ashley Friend ${new Date().getFullYear()}`,
+            managingEditor: 'Josh & Ashley Friend',
+            custom_namespaces: {
+              'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+            },
+            custom_elements: [
+              {
+                image: {
+                  url: '',
+                  title: '',
+                  link: siteMetadata.podcastInformation.url,
+                }
+              },
+              { 'itunes:author': authors },
+              { 'itunes:subtitle': 'A show about everything' },
+              { 'itunes:summary': 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our podcast in the Podcasts app or in the iTunes Store' },
+              { 'itunes:keywords': '' },
+              {
+                'itunes:owner': [
+                  { 'itunes:name': 'Josh Friend' },
+                  { 'itunes:email': 'stressedtoblessedparents@gmail.com' }
+                ]
+              },
+              { 'itunes:explicit': 'clean' },
+              {
+                'itunes:image': {
+                  _attr: {
+                    href: 'http://example.com/podcasts/everything/AllAboutEverything.jpg'
+                  }
+                }
+              },
+              {
+                'itunes:category': [
+                  { _attr: { text: 'Technology' } },
+                  { 'itunes:category': { _attr: { text: 'Gadgets' } } }
+                ]
+              },
+              { 'itunes:type' : 'episodic' },
+            ]
+          };
+
+          return {
+            ...podcastMetadata,
+            ...rest,
+          };
+        },
         feeds: [
           {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
@@ -59,10 +124,25 @@ module.exports = {
 
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
-                  copyright: `Josh & Ashley Friend ${new Date().getFullYear()}`,
                   url,
                   guid: url,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
+                  custom_elements: [
+                    { "content:encoded": edge.node.html },
+                    { enclusure: [
+                      { _attr: {
+                        length: '',
+                        type: 'audio/mpeg',
+                        url: edge.node.frontmatter.audioUrl,
+                      } },
+                    ]},
+                    { 'itunes:image': '' },
+                    { 'itunes:duration': '' },
+                    { 'itunes:explicity': 'clean' },
+                    { 'itunes:keywords': '' },
+                    { 'itunes:subtitle': '' },
+                    { 'itunes:episodeType': 'full' },
+                    { 'itunes:author': authors },
+                  ],
                 });
               });
             },
@@ -82,6 +162,7 @@ module.exports = {
                       templateKey
                       date(formatString: "MMMM DD, YYYY")
                       path
+                      audioUrl
                     }
                   }
                 }
