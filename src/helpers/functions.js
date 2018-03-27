1bee6d01a7c3b8f7141b5ec6021de14549f5fb03
Joshua Friend <josh.friend@daveramsey.com>
2018-03-26 21:32:42 -0500

    Double star? I think not! ✨

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,4 +1,4 @@
-export function trimLeft(string, replace = '\/*') {
+export function trimLeft(string, replace = '\/') {
   let regex = new RegExp(`/^(${replace}*)(.*)/`)
   return string.replace(regex, '$2');
 }
